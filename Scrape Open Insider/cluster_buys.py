b936c6d738e82baada6e847086f24d5a4f2234be
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-06-06 12:11:52 -0400

    commit

--- Scrape Open Insider/cluster_buys.py
+++ Scrape Open Insider/cluster_buys.py
@@ -7,27 +7,18 @@ driver = webdriver.Safari()
 driver.get(main_url)
 driver.implicitly_wait(10)
 
-# Find the table by its class name
 table = driver.find_element(By.CLASS_NAME, 'tinytable')
 
-# Extract the table rows
 rows = table.find_elements(By.TAG_NAME, 'tr')
-
-# Extract the data from the rows
 data = []
-for row in rows:  # Include the header row for column names
+for row in rows:
     cells = row.find_elements(By.TAG_NAME, 'td')
-    if not cells:  # If no 'td' found, check for 'th' (header)
+    if not cells:
         cells = row.find_elements(By.TAG_NAME, 'th')
     data.append([cell.text for cell in cells])
 
-# Close the browser
 driver.quit()
 
-# Convert the data to a DataFrame
-df = pd.DataFrame(data[1:], columns=data[0])  # Use the first row as header
-
-# Display the DataFrame
+df = pd.DataFrame(data[1:], columns=data[0])
 df.to_csv("test.csv")
 print(df.head())
-
